1454cf383dd52c7ab4cdd5425366802b4161d336
LaThaiBinh <nd.binhbu@gmail.com>
2018-12-21 20:38:25 +0700

    menu-bar update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FoodComponent } from './food/food.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuBottomComponent } from './menu-bottom/menu-bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodComponent,
     HeaderComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    MenuBottomComponent
   ],
   imports: [
     BrowserModule
